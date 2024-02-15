0f087a6eb1dbb374f396f0dd9d98463fc64d4e32
Arjun Bala <er.arjunbala@gmail.com>
2024-02-15 12:52:56 +0530

    Topic: Package Demo

---
+++ FacultyAttendance.java
@@ -0,0 +1,7 @@
+package faculty;
+class Attendance{
+	public static void main(String[] args) {
+		System.out.println("Faculty Attendance");
+		System.out.println("name of the student is = "+student.Attendance.name);
+	}
+}
